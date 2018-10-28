945b4188cbda7756134a6d163c74b9c2f271e163
Humberto <humbertopech30@gmail.com>
2018-10-28 11:39:11 -0600

    Clase conexion

---
+++ config/Conexion.php
@@ -0,0 +1,16 @@
+<?php
+require('config.php');
+
+class Conexion{
+    protected $conexion_db;
+
+    public function Conexion(){
+        $this->conexion_db = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRA, DB_NOMBRE);
+        id($this->conexion_db->connect_errno){
+            echo "Fallo al conectar a MySQL: " .$this->conexion_db->connect_error;
+            return;
+        }
+        $this->conexion_db->set_charset(DB_CHARSET);
+    }
+}
+?>
