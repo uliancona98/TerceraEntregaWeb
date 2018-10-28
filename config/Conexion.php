caba3f6813ae989635f61c6d3113f6c36b91a1d3
Humberto <humbertopech30@gmail.com>
2018-10-28 15:47:25 -0600

    if

--- config/Conexion.php
+++ config/Conexion.php
@@ -6,8 +6,8 @@ class Conexion{
 
     public function Conexion(){
         $this->conexion_db = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRA, DB_NOMBRE);
-        id($this->conexion_db->connect_errno){
-            echo "Fallo al conectar a MySQL: " .$this->conexion_db->connect_error;
+        if($this->conexion_db->connect_errno){
+            echo "Fallo al conectar a MySQL: " //.$this->conexion_db->connect_error;
             return;
         }
         $this->conexion_db->set_charset(DB_CHARSET);
