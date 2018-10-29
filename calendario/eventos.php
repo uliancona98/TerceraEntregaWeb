2c02e01a8fb7fc0cd8d986af316ad1178ec90a25
Humberto <humbertopech30@gmail.com>
2018-10-29 04:30:56 -0600

    agregarEvento

--- calendario/eventos.php
+++ calendario/eventos.php
@@ -2,14 +2,42 @@
 //para la conexion de BD
 header('Content-Type: application/json');
 
-$pdo = new PDO("mysql:dbname = eventos; host=localhost", "root","");
+require("../config/Conexion.php");
 
-//Seleccionar los eventos del calendario
-$sentenciaSQL = $pdo->prepare("SELECT * FROM eventos");
+$con = new Conexion();
+$conexion = $con->get_conexion();
 
-$sentenciaSQL->execute();
+$accion = (isset($_GET['accion']))?$_GET['accion']:'leer';
 
-$resultado = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+switch($accion){
+    case 'agregar':
+        $title = $_POST["title"];
+        $descripcion = $_POST['descripcion'];
+        $color = $_POST['color'];
+        $textColor = $_POST['textColor'];
+        $start = $_POST['start'];
+        $end = $_POST['end'];
+        $sentencia = "INSERT INTO EVENTOS(title, descripcion, color, textColor, start, end) VALUES($title, $descripcion, $color,$textColor,$start,$end)";
+        $conexion->query($sentencia);
+        $respuesta = $conexion->affected_rows;
+        $con->close_conexion();
+        echo json_encode($respuesta);
+        //instruccion de agregar
+        break;
+    case 'eliminar':
+        //instrucciones para eliminar
+        break;
+    case 'modificar':
+        //instrucciones para modificar
+        break;
+    default:
+        $sentencia = "SELECT * FROM EVENTOS";
+        $sentenciaSQL = $conexion->query($sentencia);
+        $resultado = $sentenciaSQL->fetch_all(MYSQLI_ASSOC);
+        $con->close_conexion();
+        echo json_encode($resultado);
+        break;
+}
 
-echo json_decode($resultado);
+
 ?>
