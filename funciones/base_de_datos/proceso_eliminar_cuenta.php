0061a14fa24ff79ae195a262573df0bf342aa848
Ulises Ancona <uliancona@hotmail.com>
2018-10-30 16:12:59 -0600

    Correcion de direcciones

--- funciones/base_de_datos/proceso_eliminar_cuenta.php
+++ funciones/base_de_datos/proceso_eliminar_cuenta.php
@@ -1,5 +1,5 @@
 <?php
-    include("conexion.php");
+    include("../../config/conexion2.php");
     session_start();
     $id = $_SESSION['id_usuario'];
     $query1 = "DELETE from publicaciones WHERE id_usuario='$id'";
@@ -9,13 +9,13 @@
         header("Location: informacion_perfil.php");
     }
     
-    $query2 = "DELETE from usuarios WHERE id='$id'";
+    $query2 = "DELETE from usuarios WHERE id_usuario='$id'";
     $resultado2 = $conexion->query($query2);
     if($resultado2){
-        header("Location: index.php");
+        header("Location:../../inicio.php");
         echo "Si se elimino";
     }else{
-        header("Location: informacion_perfil.php");
+        header("Location:../../paginas/paginas_usuarios/informacion_perfil.php");
         echo "No se elimino";
     }
 
