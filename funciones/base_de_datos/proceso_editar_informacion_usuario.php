30f3aafc9c25bc634abdf380542087958204e39d
Ulises Ancona <uliancona@hotmail.com>
2018-10-30 16:11:26 -0600

    Correcion direcciones

--- funciones/base_de_datos/proceso_editar_informacion_usuario.php
+++ funciones/base_de_datos/proceso_editar_informacion_usuario.php
@@ -1,14 +1,14 @@
 <?php
-	include("conexion.php");
+        include("../../config/conexion2.php");
         session_start();   
 	$id = $_SESSION['id_usuario'];
 	$nombre = $_SESSION['nombre'];
         $contrasena = $_SESSION['contrasenaNueva'];
                 
-	$query = "UPDATE usuarios SET nombre='$nombre', contrasena = '$contrasena' WHERE id='$id'";
+	$query = "UPDATE usuarios SET nombre='$nombre', contrasena = '$contrasena' WHERE id_usuario='$id'";
         $resultado = $conexion->query($query);
 
-        header("Location: informacion_perfil.php");
+        header("Location:../../paginas/paginas_usuarios/informacion_perfil.php");
 
 
 ?>
